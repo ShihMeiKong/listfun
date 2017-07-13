38a48641284f549036c54510ab0ebe088998061c
Pat Esposito <pespo@bluedogtec.com>
2017-07-12 23:13:23 -0400

    added list item create & delete functions - not everyting is working'

--- listfun/Scripts/App.js
+++ listfun/Scripts/App.js
@@ -33,25 +33,15 @@ function initializePage()
 }
 
 // var siteUrl = '/sites/MySiteCollection';
-console.log("Things are happppening")
-
-
-var siteUrl = '/sites/espodev/listfun';
-    // https://higherwiretechnologies-6553ca70b9b634.sharepoint.com/sites/dev/listfun
-
-// $('#okay').click(retrieveListItems)
+console.log("SP List Manipulation....Things are happppening!")
 
 var collListItem;
 
 function retrieveListItems(e) {
-        console.log("START: RetrieveListItem()")
+            console.log("START: RetrieveListItem()")
 
-   //  e.preventDefault()
-            //var clientContext = new SP.currentContext();
-            //var ctx = new SP.ClientContext.get_current();
-
-        console.log("RetrieveListItem(): Getting Curent Context & List Content")
-            var clientContext = new SP.ClientContext(siteUrl);
+            console.log("RetrieveListItem(): Getting Curent Context & List Content")
+            var clientContext = new SP.ClientContext.get_current();
             var oList = clientContext.get_web().get_lists().getByTitle('Announcements');
 
             console.log("RetrieveListItem(): Curent Context & List Content Retreived")
@@ -64,14 +54,14 @@ function retrieveListItems(e) {
             console.log("RetrieveListItem(): CAML Query Formed")
             clientContext.load(collListItem);
             console.log("RetrieveListItem(): Query Context loaded")
-            clientContext.executeQueryAsync(onQuerySucceeded, onQueryFailed);
+            clientContext.executeQueryAsync(RLIonQuerySucceeded, RLIonQueryFailed);
                 // Function.createDelegate(this, this.onQuerySucceeded),
                 // Function.createDelegate(this, this.onQueryFailed));
 
             console.log("END: RetrieveListItem()")
         };
 
-function onQuerySucceeded(sender, args) {
+function RLIonQuerySucceeded(sender, args) {
     console.log("START: onQuerySucceeded()")
 
     console.log("onQuerySucceeded(): load list")
@@ -94,7 +84,89 @@ function onQuerySucceeded(sender, args) {
     console.log("END: onQuerySucceeded()")
 }
 
-function onQueryFailed(sender, args) {
+function RLIonQueryFailed(sender, args) {
 
     alert('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
 }
+
+function createListItem() {
+    console.log("START: createListItem()")
+
+    console.log("createListItem(): Get Site and List context")
+    var clientContext = new SP.ClientContext.get_current();
+    var oList = clientContext.get_web().get_lists().getByTitle('Announcements');
+
+    console.log("createListItem(): Create List Object")
+    var itemCreateInfo = new SP.ListItemCreationInformation();
+
+    console.log("createListItem(): Create List item")
+    this.oListItem = oList.addItem(itemCreateInfo);
+
+    console.log("createListItem(): Set Values in List item")
+    oListItem.set_item('Title', 'My New Item!');
+    oListItem.set_item('Body', 'Hello World!');
+
+    console.log("createListItem(): Update List Item")
+    oListItem.update();
+
+    console.log("createListItem(): Load and Execute")
+    clientContext.load(oListItem);
+    clientContext.executeQueryAsync(Function.createDelegate(this, this.CLIonQuerySucceeded), Function.createDelegate(this, this.CLIonQueryFailed));
+
+    console.log("END: createListItem()")
+}
+
+function CLIonQuerySucceeded() {
+    console.log("START: CLIonQuerySucceeded()")
+    alert('Item created: ' + oListItem.get_id());
+    console.log("END: CLIonQuerySucceeded()")
+}
+
+function CLIonQueryFailed(sender, args) {
+    console.log("START: CLIonQueryFailed()")
+    alert('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+    console.log("END: CLIonQueryFailed()")
+}
+
+function deleteListItem() {
+    console.log("START: deleteListItem()")
+
+    console.log("deleteListItem(): geting site and list contex")
+    var clientContext = new SP.ClientContext.get_current();
+    var oWebsite = clientContext.get_web(); // this isnt correct, we need to get the list here  not the web 
+    this.listTitle = 'My New Item!';
+
+    console.log("deleteListItem(): Get List item by Title")
+    this.oList = oWebsite.get_lists().getByTitle(listTitle);
+
+    console.log("deleteListItem(): Delete Object")
+    oList.deleteObject();  // this is attempting to delete the entire list not a list item another bug from MS - bastards!
+
+    console.log("deleteListItem(): Execute")
+    clientContext.executeQueryAsync(Function.createDelegate(this, this.DLIonQuerySucceeded), Function.createDelegate(this, this.DLIonQueryFailed));
+    console.log("END: deleteListItem()")
+}
+
+function DLIonQuerySucceeded() {
+    console.log("START: DLIonQuerySucceeded()")
+    var result = listTitle + ' deleted.';
+    alert(result);
+    console.log("END: DLIonQuerySucceeded()")
+}
+
+function DLIonQueryFailed(sender, args) {
+    console.log("START: DLIonQueryFailed()")
+    alert('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+    console.log("END: DLIonQueryFailed()")
+}
+
+function toggleCSS() {
+    console.log("START: toggleCSS()")
+ 
+    console.log("toggleCSS(): getting current site")
+    var ctx = new SP.ClientContext.get_current();
+
+    alert('Cant toggle CSS - YET!');
+
+    console.log("END: toggleCSS()")
+ }
